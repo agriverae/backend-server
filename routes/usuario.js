951ba5f01e1734e2f6e83cd6acf259a072244528
Ali Rivera Echegaray <ali.rivera@globant.com>
2018-10-03 18:02:10 -0300

    backend server

--- routes/usuario.js
+++ routes/usuario.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var bcrypt = require('bcryptjs');
 
 var app = express();
 
@@ -17,7 +18,7 @@ app.get('/', (req,res,next) => {
             return res.status(500).json({
                 ok: false,
                 mensaje: 'Error cargando usuario',
-                errores: err
+                errors: err
             })
         }
 
@@ -29,6 +30,57 @@ app.get('/', (req,res,next) => {
 });
 
 // =============================================
+// Actualizar usuario
+// =============================================
+
+app.put('/:id', (req, res) =>{
+    var id = req.params.id;
+    var body = req.body
+
+    Usuario.findById(id, (err, usuario) =>{
+
+        if(err){
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar usuario',
+                errors: err
+            })
+        }
+
+        if(!usuario){
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'El usuario con el id ' + id + ' no existe',
+                errors: {message: 'No existe un usuario con ese ID'}
+            })
+        }
+
+        usuario.nombre = body.nombre;
+        usuario.email = body.email;
+        usuario.role = body.role;
+
+        usuario.save((err, usuarioGuardado) => {
+            if(err){
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'Error al actualizar usuario',
+                    errors: err
+                })
+            }
+            usuarioGuardado.password = ':)';
+
+            res.status(200).json({
+                ok: true,
+                id: usuarioGuardado
+            })
+        })
+
+    });
+
+});
+
+
+// =============================================
 // Crear un nuevo usuario
 // =============================================
 app.post('/', (req, res) => {
@@ -38,17 +90,17 @@ app.post('/', (req, res) => {
     var usuario = new Usuario({
         nombre: body.nombre,
         email: body.email,
-        password: body.password,
+        password: bcrypt.hashSync(body.password, 10),
         img: body.img,
         role: body.role
     });
 
     usuario.save( (err, usuarioGuardado) => {
         if(err){
-            return res.status(500).json({
+            return res.status(400).json({
                 ok: false,
                 mensaje: 'Error al crear usuario',
-                errores: err
+                errors: err
             })
         }
 
@@ -59,6 +111,38 @@ app.post('/', (req, res) => {
     })
 
     
+});
+
+
+// =============================================
+// Borrar un usuario
+// =============================================
+
+app.delete('/:id', (req,res) => {
+    var id = req.params.id;
+
+    Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+        if(err){
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al borrar usuario',
+                errors: err
+            })
+        }
+
+        if(!usuarioBorrado){
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'No existe un usuario con ese id',
+                errors: {message: 'No existe un usuario con ese id'}
+            })
+        }
+
+        res.status(200).json({
+            ok: true,
+            usuario: usuarioBorrado
+        })
+    });
 });
 
 module.exports = app;
